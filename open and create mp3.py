cbf8f082eaf79befa3ea33c38809a5411173a228
Runner2819 <58637130+Runner2819@users.noreply.github.com>
2022-11-12 20:13:42 +0300

    Update open and create mp3.py

--- open and create mp3.py
+++ open and create mp3.py
@@ -4,18 +4,17 @@ import numpy as np
 def open_mp3(source):
 	"""
 	:param str source: path to mp3 file
-	:return tuple data: (array of arrays(audio channels) of samples, frame rate, sample widhth)
+	:return tuple data: (array of arrays(audio channels) of samples, frame rate, sample width)
 	"""
 	audio = AudioSegment.from_mp3(source)
 	samples = []
 	raw = audio.split_to_mono()
 	if (audio.channels == 2):
-		sample = [[],[]]
-		samples[0] = raw[0].get_array_of_samples()
-		samples[1] = raw[1].get_array_of_samples()
+		samples.append(raw[0].get_array_of_samples())
+		samples.append(raw[1].get_array_of_samples())
 	else:
 		sample = [raw[0].get_array_of_samples()]
-	return (samples, audio.frame_rate, audio.sample_widhth)
+	return (samples, audio.frame_rate, audio.sample_width)
 
 def create_mp3(place, samples, fr, sw):
 	"""
@@ -26,11 +25,11 @@ def create_mp3(place, samples, fr, sw):
 	:return: void
 	"""
 	if (len(samples) == 2):
-		ch_1 = AudioSegment(sample[0].tobytes(), frame_rate = fr, sample_width = sw, channels = 1)
-		ch_2 = AudioSegment(sample[1].tobytes(), frame_rate = fr, sample_width = sw, channels = 1)
+		ch_1 = AudioSegment(samples[0].tobytes(), frame_rate = fr, sample_width = sw, channels = 1)
+		ch_2 = AudioSegment(samples[1].tobytes(), frame_rate = fr, sample_width = sw, channels = 1)
 		audio = AudioSegment.from_mono_audiosegments(ch_1,ch_2)
 	else:
-		audio = AudioSegment(sample[0].tobytes(), frame_rate = fr, sample_width = sw, channels = 1)
+		audio = AudioSegment(samples[0].tobytes(), frame_rate = fr, sample_width = sw, channels = 1)
 	audio.export(place, format="mp3")
 
 	
