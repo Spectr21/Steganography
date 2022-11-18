5cc91dfda1d9573622efc8711b36c638fa84b09c
Runner2819 <58637130+Runner2819@users.noreply.github.com>
2022-11-18 20:18:24 +0300

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -3,6 +3,8 @@ import numpy as np
 import math
 import cmath
 
+global dtype
+
 
 def open_mp3(source):
     """
@@ -10,13 +12,13 @@ def open_mp3(source):
     :return tuple data: (array of arrays(audio channels) of samples, frame rate, sample width)
     """
     audio = AudioSegment.from_mp3(source)
-    samples = []
+    samples = np.array()
     raw = audio.split_to_mono()
     if (audio.channels == 2):
-        samples.append(raw[0].get_array_of_samples())
-        samples.append(raw[1].get_array_of_samples())
+        samples.append(np.array(raw[0].get_array_of_samples()))
+        samples.append(np.raw[1].get_array_of_samples())
     else:
-        sample = [raw[0].get_array_of_samples()]
+        samples = [raw[0].get_array_of_samples()]
     return (samples, audio.frame_rate, audio.sample_width)
 
 
@@ -44,7 +46,6 @@ def str_to_arr(s):
     """
     bits = []
     for symb in s:
-        byte = (bin(ord(symb))[2:]).zfill(8)
         for bit in (bin(ord(symb))[2:]).zfill(8):
             bits.append(int(bit))
     return bits
@@ -76,14 +77,52 @@ def split_to_segments(channel, message):
     channel.resize(segment_count * segment_width)
     segments = channel.reshape(segment_count, -1)
     return segments
+def merge_segments(segments):
+    """
+    :param segments:
+    :return:
+    """
+    channel = segments.
+    return segments
 
 
-def fourier_and_so_on(segments, segment_count):
+def fourier_and_so_on(segments, segment_count, segment_width, msg):
     """
     :param segments:
     :param segment_count:
+    :param segment_width:
+    :param msg:
     :return:
     """
-    tmp = [np.fft.rfft(segments[i]) for i in range(segment_count)]
+    waves_count = segment_width // 2 + 1  # number of waves in each segment not len(waves)
+    waves = [np.fft.rfft(segments[i]) for i in range(segment_count)]
     vphase = np.vectorize(cmath.phase)
-#     WORK IN PROGRESS
+    phases = [vphase(waves[i]) for i in range(segment_count)]
+    vabs = np.vectorize(abs)
+    amps = [vabs(waves[i]) for i in range(segment_count)]
+    delta_phases = [None] * segment_count
+    # phases[i] needs to be ndarray from numpy
+    delta_phases[0] = [0] * waves_count
+    for i in range(1, segment_count):
+        delta_phases[i] = phases[i] - phases[i - 1]
+    phase0_mod = phases[0].copy()
+    msg_bits = str_to_arr(msg)
+    for i in range(1, len(msg_bits) + 1):
+        if msg_bits[i - 1] == 1:
+            phase0_mod[waves_count - i] = math.pi / 2
+        else:
+            phase0_mod[waves_count - i] = -math.pi / 2
+    phases_mod = [None] * segment_count
+    phases_mod[0] = phase0_mod.copy()
+    for i in range(1, segment_count):
+        phases_mod[i] = delta_phases[i] + phases_mod[i - 1]
+
+    def get_complex(amp, phase):
+        return amp * cmath.exp(phase * 1j)
+
+    vwave = np.vectorize(get_complex)
+    tmp_mod = [vwave(phases_mod[i], amps[i]) for i in range(segment_count)]
+    segments_mod = [np.fft.irfft(tmp_mod[i]) for i in range(segment_count)]
+
+
+
